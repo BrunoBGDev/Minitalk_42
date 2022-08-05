e51bba8fa178de091d67c450ac0352f46b76a44d
BrunoBGDev <bruno.braga.design@gmail.com>
2022-08-05 09:27:59 -0300

    Update 0.0.2: server.c and client.c started

--- client.c
+++ client.c
@@ -6,8 +6,67 @@
 /*   By: bbraga <bruno.braga.design@gmail.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/01 11:13:23 by bbraga            #+#    #+#             */
-/*   Updated: 2022/08/01 11:13:24 by bbraga           ###   ########.fr       */
+/*   Updated: 2022/08/03 20:01:55 by bbraga           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft/libft.h"
+#include <signal.h>
+#include <stdlib.h>
+#include <unistd.h>
 
+static	void action(int sig)
+{
+	static	int	received = 0;
+
+	if (sig == SIGUSR1)
+		++received;
+	else
+	{
+		ft_putnbr_fd(received, 1);
+		ft_putchar_fd('\n', 1);
+		exit(0);
+	}
+}
+
+static	void mt_kill(int pid, char *str)
+{
+	int		count;
+	char	string;
+
+	while (*str)
+	{
+		count = 8;
+		string = *str++;
+		while (count--)
+		{
+			if (c >> i & 1)
+				kill(pid, SIGUSR2);
+			else
+				kill(pid, SIGUSR1);
+			usleep(100);
+		}
+	}
+	count = 8;
+	while (count--)
+	{
+		kill(pid, SIGUSR1);
+		usleep(100);
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc != 3 || !ft_strlen(argv[2]))
+		return (1);
+	ft_putstr_fd("Sent: ", 1);
+	ft_putnbr_fd(ft_strlen(argv[2]), 1);
+	ft_putchar('\n', 1);
+	ft_putstr_fd("Received: ", 1);
+	signal(SIGUSR1, action);
+	signal(SIGUSR2, action);
+	mt_kill(ft_atoi(argv[1]), argv[2]);
+	while (1)
+		pause();
+	return (0);
+}
--- server.c
+++ server.c
@@ -6,8 +6,19 @@
 /*   By: bbraga <bruno.braga.design@gmail.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/01 11:13:32 by bbraga            #+#    #+#             */
-/*   Updated: 2022/08/01 11:13:33 by bbraga           ###   ########.fr       */
+/*   Updated: 2022/08/05 09:26:28 by bbraga           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft/libft.h"
+#include <unistd.h>
+#include <signal.h>
 
+static	void action(int sig, siginfo_t *info, void *context)
+{
+	static int				i = 0;
+	static pid_t			client_pid = 0;
+	static unsigned	char	c = 0;
+
+
+}

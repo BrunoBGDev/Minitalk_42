44b281b23c069d32775d677c9672a9ad7d6c2977
BrunoBGDev <bruno.braga.design@gmail.com>
2022-08-12 08:29:08 -0300

    Update 0.1.1: New content to client and server

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: bbraga <bruno.braga.design@gmail.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/01 11:13:23 by bbraga            #+#    #+#             */
-/*   Updated: 2022/08/08 22:00:28 by bbraga           ###   ########.fr       */
+/*   Updated: 2022/08/11 19:33:07 by bbraga           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,9 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-static	void action(int sig)
+void	action(int sig)
 {
-	static	int	received = 0;
+	static int	received = 0;
 
 	if (sig == SIGUSR1)
 		++received;
@@ -29,44 +29,56 @@ static	void action(int sig)
 	}
 }
 
-static	void mt_kill(int pid, char *str)
+void	send_mensage(int pid, unsigned char str)
 {
-	int		count;
-	char	string;
+	int	i;
 
-	while (*str)
+	i <<= 8;
+	while (str)
 	{
-		count = 8;
-		string = *str++;
-		while (count--)
+		while (i--)
 		{
-			if (string >> count & 1)
+			if (str & i)
 				kill(pid, SIGUSR2);
 			else
-				kill(pid, SIGUSR1);
+				kill(pid, SIGUSR2);
+			usleep(100);
+		}
+		i >>= 1;
+		while (i--)
+		{
+			kill(pid, SIGUSR1);
 			usleep(100);
 		}
 	}
-	count = 8;
-	while (count--)
+}
+
+void	sig_handler(char *server_pid, char *str)
+{
+	int		pid;
+
+	pid = ft_atoi(server_pid);
+	while (*str)
 	{
-		kill(pid, SIGUSR1);
-		usleep(100);
+		send_mensage(pid, *str);
+		str++;
 	}
+	send_mensage(pid, *str);
 }
 
 int	main(int argc, char **argv)
 {
 	if (argc != 3 || !ft_strlen(argv[2]))
+	{
+		ft_printf("How to use: ./client [server pid] [string to send]\n");
 		return (1);
+	}
 	ft_putstr_fd("Sent: ", 1);
 	ft_putnbr_fd(ft_strlen(argv[2]), 1);
-	ft_putchar_fd('\n', 1);
+	ft_printf("\n");
 	ft_putstr_fd("Received: ", 1);
 	signal(SIGUSR1, action);
 	signal(SIGUSR2, action);
-	mt_kill(ft_atoi(argv[1]), argv[2]);
-	while (1)
-		pause();
+	sig_handler(argv[1], argv[2]);
 	return (0);
 }
--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: bbraga <bruno.braga.design@gmail.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/01 11:13:32 by bbraga            #+#    #+#             */
-/*   Updated: 2022/08/08 21:55:57 by bbraga           ###   ########.fr       */
+/*   Updated: 2022/08/11 18:16:34 by bbraga           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,16 @@
 #include <unistd.h>
 #include <signal.h>
 
-static	void action(int sig, siginfo_t *info, void *context)
+static void	action(int sig, siginfo_t *info, void *context)
 {
 	static int				i = 0;
 	static pid_t			client_pid = 0;
-	static unsigned	char	c = 0;
+	static unsigned char	c = 0;
 
 	(void)context;
 	if (!client_pid)
 		client_pid = info->si_pid;
-	c |= (sig == SIGUSR1);
+	c |= (sig == SIGUSR2);
 	if (++i == 8)
 	{
 		i = 0;
@@ -38,7 +38,7 @@ static	void action(int sig, siginfo_t *info, void *context)
 		kill(client_pid, SIGUSR1);
 	}
 	else
-		c <<=1;
+		c <<= 1;
 }
 
 int	main(void)
@@ -47,11 +47,11 @@ int	main(void)
 
 	ft_putstr_fd("Server PID: ", 1);
 	ft_putnbr_fd(getpid(), 1);
-	ft_putchar_fd('\n', 1);
+	ft_printf("\n");
 	s_sigaction.sa_sigaction = action;
 	s_sigaction.sa_flags = SA_SIGINFO;
-	sigaction(SIGUSR1, &s_sigaction, 0);
-	sigaction(SIGUSR2, &s_sigaction, 0);
+	sigaction(SIGUSR1, &s_sigaction, NULL);
+	sigaction(SIGUSR2, &s_sigaction, NULL);
 	while (1)
 		pause();
 	return (0);

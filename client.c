b3a1913328e6417b95841123739813e468ed837e
BrunoBGDev <bruno.braga.design@gmail.com>
2022-08-21 22:30:57 -0300

    Update 1.0.0: Client and Server working.

--- client.c
+++ client.c
@@ -6,16 +6,14 @@
 /*   By: bbraga <bruno.braga.design@gmail.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/01 11:13:23 by bbraga            #+#    #+#             */
-/*   Updated: 2022/08/11 19:33:07 by bbraga           ###   ########.fr       */
+/*   Updated: 2022/08/21 22:24:59 by bbraga           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/libft.h"
 #include <signal.h>
-#include <stdlib.h>
-#include <unistd.h>
 
-void	action(int sig)
+static void	action(int sig)
 {
 	static int	received = 0;
 
@@ -23,62 +21,91 @@ void	action(int sig)
 		++received;
 	else
 	{
-		ft_putnbr_fd(received, 1);
-		ft_putchar_fd('\n', 1);
+		ft_printf("%i\n", received);
 		exit(0);
 	}
 }
 
-void	send_mensage(int pid, unsigned char str)
+static void	send_message(int pid, char *str)
 {
-	int	i;
+	size_t	index;
 
-	i <<= 8;
-	while (str)
+	index = 0;
+	while (str[index] != '\0')
 	{
-		while (i--)
-		{
-			if (str & i)
-				kill(pid, SIGUSR2);
-			else
-				kill(pid, SIGUSR2);
-			usleep(100);
-		}
-		i >>= 1;
-		while (i--)
-		{
+		if (str[index] == '1')
 			kill(pid, SIGUSR1);
-			usleep(100);
-		}
+		else
+			kill(pid, SIGUSR2);
+		index++;
+		usleep(500);
 	}
 }
 
-void	sig_handler(char *server_pid, char *str)
+static int	check_pid(char *str)
 {
-	int		pid;
+	int		index;
 
-	pid = ft_atoi(server_pid);
-	while (*str)
+	while (str[index] != '\0')
 	{
-		send_mensage(pid, *str);
-		str++;
+		if (ft_isdigit(str[index]) == 0)
+		{
+			ft_printf("ERROR:Pid require only numbers!\n");
+			return (1);
+			break ;
+		}
+		else
+			index++;
 	}
-	send_mensage(pid, *str);
+	return (0);
+}
+
+static char	*str_to_bin(char *str, size_t count, size_t index)
+{
+	char	*rtn;
+	int		chr;
+	int		bit;
+
+	count = ft_strlen(str);
+	rtn = ft_calloc(count * 8 + 1, sizeof(char));
+	if (!rtn)
+		return (0);
+	while (count + 1 != 0)
+	{
+		chr = str[count - 1];
+		bit = 8;
+		while (bit > 0)
+		{
+			if (chr % 2 == 1)
+				rtn[ft_strlen(str) * 8 - index - 1] = '1';
+			else
+				rtn[ft_strlen(str) * 8 - index - 1] = '0';
+			chr /= 2;
+			index++;
+			bit--;
+		}
+		count--;
+	}
+	return (rtn);
 }
 
 int	main(int argc, char **argv)
 {
+	char	*bin;
+	int		pid;
+
+	if (check_pid(argv[1]) == 1)
+		return (1);
 	if (argc != 3 || !ft_strlen(argv[2]))
 	{
-		ft_printf("How to use: ./client [server pid] [string to send]\n");
+		ft_printf("ERROR:How to use: ./client [server pid] [string to send]\n");
 		return (1);
 	}
-	ft_putstr_fd("Sent: ", 1);
-	ft_putnbr_fd(ft_strlen(argv[2]), 1);
-	ft_printf("\n");
-	ft_putstr_fd("Received: ", 1);
-	signal(SIGUSR1, action);
+	pid = ft_atoi(argv[1]);
+	bin = str_to_bin(argv[2], 0, 0);
+	ft_printf("Sent: %i\n", ft_strlen(argv[2]));
+	ft_printf("Received: ");
 	signal(SIGUSR2, action);
-	sig_handler(argv[1], argv[2]);
+	send_message(pid, bin);
 	return (0);
 }
--- server.c
+++ server.c
@@ -6,52 +6,89 @@
 /*   By: bbraga <bruno.braga.design@gmail.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/01 11:13:32 by bbraga            #+#    #+#             */
-/*   Updated: 2022/08/11 18:16:34 by bbraga           ###   ########.fr       */
+/*   Updated: 2022/08/21 22:22:32 by bbraga           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/libft.h"
-#include <unistd.h>
 #include <signal.h>
+
+static void	ft_convert_bin(char *str)
+{
+	unsigned char	chr;
+	size_t			len;
+	char			*string;
+	int				temp;
+
+	string = str;
+	temp = 1;
+	chr = 0;
+	len = ft_strlen(string) - 1;
+	while (len + 1 != 0)
+	{
+		chr += temp * (string[len] - '0');
+		temp *= 2;
+		len--;
+	}
+	write(1, &chr, 1);
+}
+
+static char	*ft_minijoin(char *start, char str)
+{
+	size_t	index;
+	char	*tmp;
+
+	tmp = malloc(ft_strlen(start) + 2);
+	if (!tmp)
+		return (0);
+	index = 0;
+	while (start[index] != '\0')
+	{
+		tmp[index] = start[index];
+		index++;
+	}
+	tmp[index] = str;
+	tmp[index + 1] = '\0';
+	free(start);
+	return (tmp);
+}
 
 static void	action(int sig, siginfo_t *info, void *context)
 {
-	static int				i = 0;
-	static pid_t			client_pid = 0;
-	static unsigned char	c = 0;
+	static char			*bits;
+	static int			bitcount;
+	static pid_t		client_pid = 0;
 
 	(void)context;
 	if (!client_pid)
 		client_pid = info->si_pid;
-	c |= (sig == SIGUSR2);
-	if (++i == 8)
+	bitcount++;
+	if (bits == NULL)
 	{
-		i = 0;
-		if (!c)
-		{
-			kill(client_pid, SIGUSR2);
-			client_pid = 0;
-			return ;
-		}
-		ft_putchar_fd(c, 1);
-		c = 0;
-		kill(client_pid, SIGUSR1);
+		bits = ft_strdup("");
+		bitcount = 1;
 	}
+	if (sig == SIGUSR2)
+		bits = ft_minijoin(bits, '0');
 	else
-		c <<= 1;
+		bits = ft_minijoin(bits, '1');
+	if (bitcount == 8)
+	{
+		ft_convert_bin(bits);
+		free(bits);
+		bits = NULL;
+	}
 }
 
 int	main(void)
 {
 	struct sigaction	s_sigaction;
 
-	ft_putstr_fd("Server PID: ", 1);
-	ft_putnbr_fd(getpid(), 1);
-	ft_printf("\n");
+	ft_printf("Server PID: %u\n", getpid());
 	s_sigaction.sa_sigaction = action;
 	s_sigaction.sa_flags = SA_SIGINFO;
-	sigaction(SIGUSR1, &s_sigaction, NULL);
-	sigaction(SIGUSR2, &s_sigaction, NULL);
+	sigaction(SIGUSR1, &s_sigaction, 0);
+	sigaction(SIGUSR2, &s_sigaction, 0);
 	while (1)
 		pause();
 	return (0);
